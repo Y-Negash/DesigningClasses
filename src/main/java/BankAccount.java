aec14570bf824d27c609769688c276725712ba9d
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-02-07 15:27:27 +0000

    add getters and setters

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -14,5 +14,46 @@ public BankAccount(String firstName, String lastName, String dateOfBirth, int ac
     this.accountNumber = accountNumber;
     this.balance = 0;
 }
+// GETTERS AND SETTERS
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 
 }
--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -1,17 +1,22 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BankAccountTest {
 
     private BankAccount bankAccount;
 
     @BeforeEach
-    public  void setUp() { bankAccount = new BankAccount("Yabbi","qw", "12/24/4",9)}
+    public  void setUp() { bankAccount = new BankAccount("Yabbi","qw", "12/24/4",9);
 
-    // object
+   
+
     @Test
-    public void canGetFirstName(){ //calling method on the object
-
+    public void canGetFirstName(){
+    String actual = bankAccount.getFirstName();
+    String expected = "Yabbi";
+    assertThat(actual).isEqualTo(expected);
 
 
 
