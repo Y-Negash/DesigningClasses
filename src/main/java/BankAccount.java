7bf4f6f5c69ec6524c9696f2f1e40a848e5a3787
Yeabsira Negash <n.yeabsira.a@gmail.com>
2024-02-07 15:15:57 +0000

    first commit

---
+++ src/main/java/BankAccount.java
@@ -0,0 +1,18 @@
+public class BankAccount {
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private int accountNumber;
+    private int balance;
+
+//    Constructor
+public BankAccount(String firstName, String lastName, String dateOfBirth, int accountNumber){
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+    this.accountNumber = accountNumber;
+    this.balance = 0;
+}
+
+}
---
+++ src/test/java/BankAccountTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BankAccountTest {
+
+    private BankAccount bankAccount;
+
+    @BeforeEach
+    public  void setUp() { bankAccount = new BankAccount("Yabbi","qw", "12/24/4",9)}
+
+    // object
+    @Test
+    public void canGetFirstName(){ //calling method on the object
+
+
+
+
+    }
+}
